27c74add257966d6776c10e25aa6180d439d4159
jovanodave <jovanodave@gmail.com>
2025-02-05 19:06:05 +0700

    Update Marketplace.py

--- pages/Marketplace.py
+++ pages/Marketplace.py
@@ -36,9 +36,9 @@ for i in range(len(df)):
             st.write(f'{record['Product_Name']}')
             st.write(f'{record['Price']}')
             st.write(f'{record['Description']}')
-            if st.button("Add to Cart"):
+            if st.button1("Add to Cart"):
                 st.write("Added to Cart")
-            if st.button("Buy"):
+            if st.button2("Buy"):
                 st.write("Thank you")
 
 # st.dataframe(df)
