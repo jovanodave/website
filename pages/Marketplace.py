df7ac781b1c5a7239fa126adf557fa59438c87e0
jovanodave <jovanodave@gmail.com>
2025-02-18 19:00:52 +0700

    Update Marketplace.py

--- pages/Marketplace.py
+++ pages/Marketplace.py
@@ -30,37 +30,26 @@ with pr:
     highest_price = (df['Price'].max())
     selection_price = st.slider("Price Range", min_value = lowest_price, max_value = highest_price)
 
-num_of_columns = 5 # create variable to set the column
+num_of_columns = st.number_input("How many columns:",value = 4,step=1) # create variable to set the column
 columns = st.columns(num_of_columns) # create the column
 data_length = len(df)
 #num_of_rows = int(data_length / num_of_columns)
 
 for i in range(data_length):
-    if i%num_of_columns == 0:
-        col = columns[0]
-    if i%num_of_columns == 1:
-        col = columns[1]
-    if i%num_of_columns == 2:
-        col = columns[2]
-    if i%num_of_columns == 3:
-        col = columns[3]
-    if i%num_of_columns == 4:
-        col = columns[4]
-
-    with col:
-        with st.container(border=True):
-            record = df.iloc[i]
-            st.image(f'{record['Picture']}',width=250)
-            st.write(f'{record['Product_Name']}')
-            st.write(f'{record['Price']}')
-            st.write(f'{record['Description']}')
-            if st.button("Add to Cart",key=f'{i}'):
-                st.write("Added to Cart")
-            if st.button("Buy",key=f'buy{i}'):
-                st.write("Thank You :D")
+    for r in range(num_of_columns):
+        if i%num_of_columns == r:
+            col = columns[r]
+            
+            with col:
+                with st.container(border=True):
+                    record = df.iloc[i]
+                    st.image(f'{record['Picture']}',width=250)
+                    st.write(f'{record['Product_Name']}')
+                    st.write(f'{record['Price']}')
+                    st.write(f'{record['Description']}')
+                    if st.button("Add to Cart",key=f'{i}'):
+                        st.write("Added to Cart")
+                    if st.button("Buy",key=f'buy{i}'):
+                        st.write("Thank You :D")
 
 # st.dataframe(df)
-
-#st.write(df.columns)
-#st.write(df['Category'].unique())
-#st.write(df['Store'].unique())
