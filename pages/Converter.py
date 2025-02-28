fbdd6738c276b229381b498456d36fc60c8d0788
jovanodave <jovanodave@gmail.com>
2025-02-28 19:03:29 +0700

    Update Converter.py

--- pages/Converter.py
+++ pages/Converter.py
@@ -2,7 +2,23 @@ import streamlit as st
 
 st.title("Unit Converter")
 
-factor = {'mm' : 1, 'cm' : 0.1, 'm' : 0.001, 'km' : 0.000001}
+coversion_factor = {
+                    'distance': {
+                        'mm' : 1,
+                        'cm' : 0.1, 
+                        'm' : 0.001, 
+                        'km' : 0.000001
+                        'inch' : 0.393701
+                        'feet' : 0.00328084
+                        'yards' : 0.00109361
+                        'miles' : 6.2137e-7},
+                    'weight' : {
+                        'gr' : 1,
+                        'kg' : 0.1, 
+                        'mg' : 0.001, 
+                        'ton' : 0.000001
+                        'pound' : 0.393701
+                        'ounce' : 0.00328084}}
 
 with st.container(border=True):
     st.write("")
