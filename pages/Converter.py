8368b2c5f6b2bb2a6848dd46bec388a199a11c86
jovanodave <jovanodave@gmail.com>
2025-03-11 18:16:12 +0700

    Update Converter.py

--- pages/Converter.py
+++ pages/Converter.py
@@ -1,7 +1,7 @@
 import streamlit as st
+st.set_page_config(layout='wide')
 
 st.title("Unit Converter")
-st.set_page_config(layout='wide')
 
 conversion_factor = {
                     'distance': {
